71426d1e8374336f28e837375560be60da1a7c6c
Vasco Lameiras <lameiras@gmail.com>
2023-04-09 00:57:58 +0100

    add pre-commit script

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -euo pipefail
+
 latest_commit=$(git rev-parse HEAD)
 
 # Stash changes not staged for commit, so that we only analyze staged changes
@@ -6,11 +9,12 @@ latest_commit=$(git rev-parse HEAD)
 #diff_output=$(git diff "$latest_commit" HEAD)
 changed_files=$(git diff --name-only "$latest_commit" HEAD)
 
+echo "$changed_files"
 file_contents=""
 for file in $changed_files; do
     if [ -f "$file" ]; then
         content=$(cat "$file")
         file_contents+=$"==== File: ${file} ====\n${content}\n\n"
-        ech $file_contents
+        echo "$file_contents"
     fi
 done
