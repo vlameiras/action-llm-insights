066e5604820ed2b3e0c3d8fa0863fda5d08c4d4b
Vasco Lameiras <lameiras@gmail.com>
2023-04-13 15:33:30 +0100

    Update README. Add LICENSE

---
+++ README.md
@@ -0,0 +1,56 @@
+# LLM Insights
+
+LLM Insights is a tool that utilizes OpenAI's `text-davinci-003` to analyze your git diffs and provide potential optimizations, observations, bugs detected, and suggested fixes. This tool is designed to help developers improve their codebase by providing intelligent insights based on the latest changes.
+
+## Installation
+Make sure you have Python 3.6 or higher installed on your system. You can check your Python version with the following command:
+
+```bash
+python --version
+```
+
+Install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+Set up an OpenAI API key by following the instructions here. After obtaining the API key, set it as an environment variable:
+
+```bash
+export OPENAI_API_KEY=your_api_key_here
+```
+
+Add the pre-commit hook to your git repository. Place the provided `pre-commit.sh` file in the .git/hooks folder of your repository and rename it to pre-commit. Make sure the file is executable:
+
+```bash
+chmod +x .git/hooks/pre-commit
+```
+
+## Usage
+The LLM Insights tool will automatically run when you execute git commit. It will analyze the changes in the staged files and provide insights, bug detections, and fixes based on the output from OpenAI's GPT-4. If any bugs are detected, the tool will apply the suggested fixes and ask you to review and stage the changes before committing.
+
+## Files
+
+### llm_insights.py
+
+This is the main script that takes the git diff and changed file contents as input and uses the OpenAI API to get code insights. The insights are then printed to the console for the developer to review.
+
+### pre-commit.sh
+
+This is the pre-commit hook script that runs before a commit is made. It gathers the git diff and changed file contents and calls the llm_insights.py script to get insights and fixes. If any bugs are detected and fixed, it will prevent the commit, allowing the developer to review and stage the changes before committing.
+
+## Troubleshooting
+
+If you encounter any issues while using LLM Insights, check the following:
+
+- Ensure your OpenAI API key is set correctly as an environment variable.
+- Verify that the `llm_insights.py` script and the `pre-commit.sh` file have the correct file permissions.
+- Confirm that the required Python dependencies are installed.
+- Check if the pre-commit hook is correctly placed in the .git/hooks folder and renamed to pre-commit.
+
+If you still encounter issues, please consult the error messages for more information or consider opening an issue on the project repository.
+
+## License
+
+LLM Insights is licensed under the terms of the MIT license. See the [LICENSE](LICENSE) file for more information.
--- llm_insights.py
+++ llm_insights.py
@@ -2,47 +2,61 @@ import os
 import sys
 import openai
 
+# Set API key
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# get the latest git staged files and diff
+
 def get_git_diff():
-    # get the latest commit
-    print("Getting latest git commit...")
-    commit = os.popen("git rev-parse HEAD").read().strip()
+    try:
+        # Get latest commit
+        print("Getting latest git commit...")
+        commit = os.popen("git rev-parse HEAD").read().strip()
 
-    # get the latest git diff between staged files and HEAD
-    print("Getting latest git diff...")
-    diff = os.popen(f"git diff --cached {commit}").read()
+        # Get latest git diff between staged files and HEAD
+        print("Getting latest git diff...")
+        diff = os.popen(f"git diff --cached {commit}").read()
 
-    # get the latest git staged files
-    print("Getting latest git staged files...")
-    staged_files = os.popen("git diff --name-only --cached").read().splitlines()
+        # Get latest git staged files
+        print("Getting latest git staged files...")
+        staged_files = os.popen("git diff --name-only --cached").read().splitlines()
 
-    return diff, staged_files
+        return diff, staged_files
     
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+        
 
 def get_code_insights(diff, file_contents):
-    # use openai to get code insights
-    print("Getting code insights from OpenAI...")
-    
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=(
-            f"Analyze the following code diff and the contents of the changed files."
-            f"Provide potential optimizations, observations, bugs detected, and suggested fixes. Present code snippets for bugs and fixes:\n\n"
-            f"--- Code Diff ---\n"
-            f"{diff}\n\n"
-            f"--- Changed Files ---\n"
-            f"{file_contents}\n"
-        ),
-        temperature=0.001,
-        max_tokens=300,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-    
-    return response.choices[0].text.strip()
+
+    try:
+        # Get code insights using OpenAI
+        print("Getting code insights from OpenAI...")
+        
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=(
+                f"Analyze the following code diff and the contents "
+                f"of the changed files. Provide potential optimizations, "
+                f"observations, bugs detected, and suggested fixes. All "
+                f"the insights should be detailed and relevant for the developer. "
+                f"Present code snippets for bugs and fixes:\n\n"
+                f"--- Code Diff ---\n{diff}\n\n"
+                f"--- Changed Files ---\n{file_contents}\n"
+            ),
+            temperature=0.1,
+            max_tokens=300,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+        
+        return response.choices[0].text.strip()
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     diff, file_contents = get_git_diff()
