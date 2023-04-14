7c5810631163c8cd1d054fd308addf76547da6a8
Vasco Lameiras <lameiras@gmail.com>
2023-04-14 11:15:33 +0100

    Testing hook

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 def prime_numbers():
     for num in range(1, 1000):
         if num > 1:
-            for i in range(2, num, 3):
+            for i in range(x, num):
                 if (num % i) == 0:
                     break
             else:
