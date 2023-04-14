554db0ad892f6c51b1bf3848ab03371e3ee67ea7
Vasco Lameiras <lameiras@gmail.com>
2023-04-14 11:14:47 +0100

    Testing hook

---
+++ test.py
@@ -0,0 +1,10 @@
+# A function that generates prime numbers from 1 to 1000
+
+def prime_numbers():
+    for num in range(1, 1000):
+        if num > 1:
+            for i in range(2, num, 3):
+                if (num % i) == 0:
+                    break
+            else:
+                print(num)
